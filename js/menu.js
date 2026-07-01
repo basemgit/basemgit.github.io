d1da8cca8aa460b0dc1083ec5c362592de92f3a3
Basem Ibrahim <basem1ibrahim@gmail.com>
2026-07-01 09:52:39 +0300

    menu 2

--- js/menu.js
+++ js/menu.js
@@ -2,14 +2,19 @@ const menuButton = document.querySelector(".menu-button");
 const closeButton = document.querySelector(".close-button");
 const sideMenu = document.querySelector(".side-menu");
 
-menuButton.addEventListener("click", () => {
-
+menuButton.addEventListener("click", (event) => {
+    event.stopPropagation();
     sideMenu.classList.add("open");
-
 });
 
 closeButton.addEventListener("click", () => {
-
     sideMenu.classList.remove("open");
+});
 
+sideMenu.addEventListener("click", (event) => {
+    event.stopPropagation();
+});
+
+document.addEventListener("click", () => {
+    sideMenu.classList.remove("open");
 });
