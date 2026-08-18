ee611244615f503d229bf21909ea3290cd3728c4
Basem Ibrahim <basem1ibrahim@gmail.com>
2026-08-18 20:08:13 +0300

    Update music.js

--- js/music.js
+++ js/music.js
@@ -10,7 +10,7 @@ const songs = [
     { title: "Your Lane", image: "images/music/your-lane.webp", link: "https://push.fm/fl/q7bnqzep" },
     { title: "Wake Up", image: "images/music/wake-up.webp", link: "https://push.fm/fl/khornipj" },
     { title: "Don't Stop", image: "images/music/dont-stop.webp", link: "https://push.fm/fl/7dvkiprp" },
-    { title: "Nonsense", image: "images/music/nonsense.webp", link: "https://distrokid.com/hyperfollow/besomusic/nonsense" },
+    { title: "Nonsense", image: "images/music/nonsense.webp", link: "https://push.fm/fl/mvz7zkvl" },
     { title: "Soap", image: "images/music/soap.webp", link: "https://distrokid.com/hyperfollow/besomusic/soap" },
     { title: "Sublime", image: "images/music/sublime.webp", link: "https://distrokid.com/hyperfollow/besomusic/sublime" },
     { title: "Choice", image: "images/music/choice.webp", link: "https://distrokid.com/hyperfollow/besomusic/choice" },
