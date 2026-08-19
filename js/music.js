0fc73b34320b46d3589a90394af33f767ca48cea
Basem Ibrahim <basem1ibrahim@gmail.com>
2026-08-19 14:55:08 +0300

    Update music.js

--- js/music.js
+++ js/music.js
@@ -42,7 +42,7 @@ const songs = [
     { title: "Joy", image: "images/music/joy.webp", link: "https://push.fm/fl/fugvqiuk" },
     { title: "It's Been A Pleasure", image: "images/music/its-been-a-pleasure.webp", link: "https://push.fm/fl/ym74acoi" },
     { title: "Pinky Swear", image: "images/music/pinky-swear.webp", link: "https://push.fm/fl/bpu8nwyb" },
-    { title: "Punch Back", image: "images/music/punch-back.webp", link: "https://distrokid.com/hyperfollow/besomusic/punch-back" },
+    { title: "Punch Back", image: "images/music/punch-back.webp", link: "https://push.fm/fl/7ns5heox" },
     { title: "Unleash", image: "images/music/unleash.webp", link: "https://distrokid.com/hyperfollow/besomusic/unleash" },
     { title: "Sniffing The Clouds", image: "images/music/sniffing-the-clouds.webp", link: "https://distrokid.com/hyperfollow/besomusic/sniffing-the-clouds" },
     { title: "Freedom", image: "images/music/freedom.webp", link: "https://distrokid.com/hyperfollow/besomusic/freedom" },
