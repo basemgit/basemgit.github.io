85bbacc8a751405d8e8cdeb0fc59f464c9481208
Basem Ibrahim <basem1ibrahim@gmail.com>
2026-08-19 08:17:57 +0300

    Update music.js

--- js/music.js
+++ js/music.js
@@ -22,7 +22,7 @@ const songs = [
     { title: "Jump", image: "images/music/jump.webp", link: "https://push.fm/fl/jdih0qxd" },
     { title: "I Told You So", image: "images/music/i-told-you-so.webp", link: "https://push.fm/fl/pr3whqwl" },
     { title: "Why?", image: "images/music/why.webp", link: "https://push.fm/fl/3wjtkxkc" },
-    { title: "Odds", image: "images/music/odds.webp", link: "https://distrokid.com/hyperfollow/besomusic/odds" },
+    { title: "Odds", image: "images/music/odds.webp", link: "https://push.fm/fl/t2tsotix" },
     { title: "A Lovely Demon", image: "images/music/a-lovely-demon.webp", link: "https://distrokid.com/hyperfollow/besomusic/a-lovely-demon" },
     { title: "Sweet Nap", image: "images/music/sweet-nap.webp", link: "https://distrokid.com/hyperfollow/besomusic/sweet-nap" },
     { title: "Bastard", image: "images/music/bastard.webp", link: "https://distrokid.com/hyperfollow/besomusic/bastard" },
